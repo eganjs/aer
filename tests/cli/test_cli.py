f9ab75132d0711e87e76fcf46e279c21d903ea78
Joseph <joseph.s.egan@gmail.com>
2020-08-13 19:20:12 +0100

    Add version flag to CLI (#3)
    * Add version flag to CLI
    * Upversion to 0.3.0

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,4 +1,6 @@
+import toml
 from click.testing import CliRunner
+from pyprojroot import here
 
 from aer.cli import cli
 
@@ -9,3 +11,14 @@ def test_hello_world():
 
     assert result.exit_code == 0
     assert result.output == "Hello, World!\n"
+
+
+def test_version_option():
+    pyproject = toml.load(here("pyproject.toml"))
+    pyproject_version = pyproject["tool"]["poetry"]["version"]
+
+    runner = CliRunner()
+    result = runner.invoke(cli, "--version")
+
+    assert result.exit_code == 0
+    assert result.output == f"main, version {pyproject_version}\n"
--- tests/test_version.py
+++ tests/test_version.py
@@ -6,5 +6,6 @@ from aer import __version__
 
 def test_version():
     pyproject = toml.load(here("pyproject.toml"))
+    pyproject_version = pyproject["tool"]["poetry"]["version"]
 
-    assert __version__ == pyproject["tool"]["poetry"]["version"]
+    assert __version__ == pyproject_version
